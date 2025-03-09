2ed3771263254c4a478ebb8df221af805025b2fb
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-09 23:11:59 +0330

    location template added

--- core/locations/views.py
+++ core/locations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Location
 
-# Create your views here.
+class LocationsView(TemplateView):
+    template_name = "locations/locations.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['locations'] = Location.objects.all()
+        return context
