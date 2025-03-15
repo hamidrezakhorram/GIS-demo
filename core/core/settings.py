640503ae899697b66f7a4b4c6eefbd67c115ffed
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-15 15:16:46 +0330

    setup DRF

--- core/core/settings.py
+++ core/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'locations',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- core/locations/urls.py
+++ core/locations/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
+
+app_name = 'locations'
 
 urlpatterns = [
     path('', views.LocationsView.as_view(), name='locations'),
+    path('api/', include('locations.api.urls')),
 ]
