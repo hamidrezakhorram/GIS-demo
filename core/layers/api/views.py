d6de12fd81ca5ceccb9c254679517f1bc0f0adf1
hamidrezakhorram <hamid.khorram.2020@gmail.com>
2025-03-16 20:27:08 +0330

    add layers API filters

--- core/layers/api/views.py
+++ core/layers/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from layers.models import Layer
 from layers.api.serializers import LayerSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.filters import SearchFilter , OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class LayerViewSet(viewsets.ModelViewSet):
     """
@@ -10,3 +12,7 @@ class LayerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Layer.objects.all()
     serializer_class = LayerSerializer
+    filter_backends = [DjangoFilterBackend , SearchFilter , OrderingFilter]
+    filterset_fields = ['layer_type']
+    search_fields = ['name']
+    ordering_fields = ['uploaded_at']
